d92b4cd75c4d6a5e730d636cb48e6b50e46b8ae3
Jessie Renton <jsr.renton@gmail.com>
2021-11-18 10:18:46 +0000

    add functions to fit cummulative VAF distribution with 1/f model

---
+++ SomaticEvolution/analyse.jl
@@ -0,0 +1,26 @@
+function analysedata(df::DataFrame, fmin, fmax)
+    df = df[fmin .<= df.VAF .< fmax, :]
+    #remove clonal mutations from the cumultive frequency
+    df[!, :cumfreq] .= df[!, :cumfreq] .- df[end, :cumfreq] .+ df[end, :freq]
+    #fit the data to y = m(x - 1/fmax), enforcing the intercept y(1/fmax) = 0
+    #y = M(f), x = 1/f and the gradient m = μ/β
+    df.invVAF = 1 ./df[!, :VAF] .- 1/fmax
+    lmfit = fit(LinearModel, @formula(cumfreq ~ invVAF + 0), df)
+    df.prediction = predict(lmfit)
+    df.residuals = residuals(lmfit)
+    df[!,:invVAF] .+= 1/fmax
+    return df, coef(lmfit)[1], r2(lmfit)  
+end
+
+function analysedata(VAF, fmin, fmax; xstep=0.001)
+    VAF = filter(x-> fmin <= x <= fmax, VAF)
+    df = gethist(VAF, xmin = fmin, xmax = fmax, xstep = xstep)
+    #fit the data to y = m(x - 1/fmax), enforcing the intercept y(1/fmax) = 0
+    #y = M(f), x = 1/f and the gradient m = μ/β
+    df.invVAF = 1 ./df[!, :VAF] .- 1/fmax
+    lmfit = fit(LinearModel, @formula(cumfreq ~ invVAF + 0), df)
+    df.prediction = predict(lmfit)
+    df.residuals = residuals(lmfit)
+    df[!,:invVAF] .+= 1/fmax
+    return df, coef(lmfit)[1], r2(lmfit)  
+end
--- SomaticEvolution/sampling.jl
+++ SomaticEvolution/sampling.jl
@@ -12,15 +12,13 @@ function sampledhist(trueVAF::Array{Float64, 1}, cellnum::Int64, rng::AbstractRN
     depth = rand(rng, Binomial(cellnum,read_depth/cellnum), length(VAF))
     # depth = rand(Poisson(read_depth), length(trueVAF)) #why is depth Poisson distributed??
     sampalleles = map((n, p) -> rand(rng, Binomial(n, p)), depth, VAF)
-
     sampledVAF = sampalleles./depth
-    dfhist = gethist(sampledVAF,0.005,0.995,0.01)
     #data for histogram
-    return SampledData(dfhist, sampledVAF, sampalleles, depth)
+    return SampledData(sampledVAF, sampalleles, depth)
 end
 
-function gethist(VAF::Array{Float64,1}, xmin=0.005, xmax=0.995, xstep=0.01)
-    x = xmin:xstep:xmax+xstep
+function gethist(VAF::Array{Float64,1}; xmin=0.0, xmax=1, xstep=0.001)
+    x = xmin:xstep:xmax
     y = fit(Histogram, VAF, x, closed=:right).weights
     dfhist = DataFrame(VAF = x[1:end-1], freq = y)
     dfhist = addcumfreq!(dfhist,:freq)
