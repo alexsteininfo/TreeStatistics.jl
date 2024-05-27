8ab19e69055da40e241fde7628e979da99be9f48
Alexander Stein <91415340+alexsteininfo@users.noreply.github.com>
2024-05-27 13:51:57 +0100

    Update simulations.jl

--- src/simulations.jl
+++ src/simulations.jl
@@ -65,7 +65,7 @@ function simulate!(
             subclone.deathrate = input.deathrate
         else
             #selectioncoefficient = selection.mutant_selection[i-1]
-            subclone.birthrate = input.birthrate - selection[i-1]
+            subclone.birthrate = input.birthrate - selection.mutant_selection[i-1]
             #subclone.deathrate = input.deathrate + selectioncoefficient
             #nothing # cells behave as before
         end
